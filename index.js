67080d8ad6bd8026ffa7cc6490f3654723e16c00
osmankiral96 <96803197+osmankiral96@users.noreply.github.com>
2022-08-20 20:07:30 +0300

    guncel

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const s3Controller = require('./src/s3-controller')
 const app = express()
 
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/',(req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
